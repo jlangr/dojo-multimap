67c901fca8ab9d19d69669bc12541c005147a44a
Jeff Langr <jeff.langr@outpace.com>
2015-11-28 11:31:04 -0700

    init

---
+++ MultiMapTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class MultiMapTest {
+   @Test
+   public void isEmptyWhenCreated() {
+   }
+}
