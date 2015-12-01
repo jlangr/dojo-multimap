9132351612e02b643058ac85c672aa603802713a
Jeff Langr <jeff.langr@outpace.com>
2015-12-01 12:04:28 -0700

    retab

--- MultiMapTest.java
+++ MultiMapTest.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class MultiMapTest {
-   @Test
-   public void isEmptyWhenCreated() {
-   }
+    @Test
+    public void isEmptyWhenCreated() {
+    }
 }
